5a3fe487216aba891fade313927ebced7f084845
Anikait <50035624+anikait1@users.noreply.github.com>
2021-02-25 15:34:07 +0530

    Removed auth0 authentication

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { auth, requiresAuth } = require("express-openid-connect");
 require("dotenv").config({ path: "./.local.env" });
 
 const app = express();
 app.use(express.json());
-app.use(
-  auth({
-    authRequired: false,
-    auth0Logout: true,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    secret: process.env.SECRET,
-  })
-);
 
 const cipherRouter = require("./resources/cipher/cipher.route");
 
@@ -29,17 +18,6 @@ mongoose.connection.on(
 mongoose.connection.once("open", () => {
   console.log("DB is up");
 });
-
-const posts = [
-  { text: "this is a random piece of text", isSolved: false },
-  { text: "more text to parse", isSolved: false },
-  { text: "another piece of text", isSolved: false },
-  { text: "will encrypt data and more", isSolved: false },
-];
-
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.json(req.oidc.user); 
-})
 
 app.use("/ciphers", cipherRouter);
 
