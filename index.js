58021f664c55f3fead29d3f5d1fcecf111fbde65
Anikait <50035624+anikait1@users.noreply.github.com>
2021-03-08 21:20:21 +0530

    Setup express session

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
 require("dotenv").config({ path: "./.local.env" });
 
 const app = express();
+app.use(
+  session({
+    name: "qid",
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false, // set in prod
+    },
+    saveUninitialized: false
+  })
+);
 app.use(express.json());
 
 const cipherRouter = require("./resources/cipher/cipher.route");
+const userRouter = require("./resources/user/user.route");
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -20,6 +36,7 @@ mongoose.connection.once("open", () => {
 });
 
 app.use("/ciphers", cipherRouter);
+app.use("/user", userRouter);
 
 app.listen(8000, () => {
   console.log("Server is up");
