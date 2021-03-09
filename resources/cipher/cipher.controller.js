65bb68ad4bf571f6850f7e402ebcacb7d85f9ca4
Anikait <50035624+anikait1@users.noreply.github.com>
2021-03-09 21:20:32 +0530

    Updated middleware and added request to fetch a single cipher

--- resources/cipher/cipher.controller.js
+++ resources/cipher/cipher.controller.js
@@ -9,13 +9,51 @@ exports.getCiphers = async (_req, res) => {
         encryptedText: 1,
         solved: 1,
         hints: 1,
+        userName: 1,
+      })
+      .lean()
+      .exec();
+    
+    console.log(typeof ciphers[0]._id);
+
+    return res.status(200).json(ciphers);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+exports.getCipherById = async (req, res) => {
+  try {
+    return res.status(200).json(req.cipher);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+exports.attachCipherToRequest = async (req, res, next) => {
+  try {
+    const cipher = await Cipher.findById(req.params.id)
+      .select({
+        userId: 1,
+        encryptedText: 1,
+        solved: 1,
+        hints: 1,
+        userName: 1,
+        text: 1,
       })
       .lean()
       .exec();
 
-    res.status(200).json(ciphers);
+    if (!cipher) {
+      return res.status(404).json({ err: "Cipher Not Found" });
+    }
+
+    req.cipher = cipher;
+    console.log(cipher);
+    next();
   } catch (err) {
-    res.json(err);
+    console.log(err);
+    res.status(422).json(err);
   }
 };
 
@@ -28,14 +66,16 @@ exports.addCipher = async (req, res) => {
 
     const cipher = new Cipher({
       ...cipherRequest,
+      userId: req.session.user.id,
+      userName: req.session.user.name,
       encryptedText,
       hints,
       solved: false,
     });
 
     await cipher.save();
-    res.status(201).json({ _id: cipher._id });
+    return res.status(201).json({ id: cipher._id });
   } catch (err) {
-    res.json({ err });
+    return res.json({ err });
   }
 };
