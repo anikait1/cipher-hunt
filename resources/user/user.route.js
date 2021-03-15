7a3434e3e574c34c4bfbb10e286068b9ec486d72
Anikait <50035624+anikait1@users.noreply.github.com>
2021-03-15 14:59:26 +0530

    Fixed route name

--- resources/user/user.route.js
+++ resources/user/user.route.js
@@ -5,7 +5,7 @@ const auth = require("../../utils/auth");
 
 router.post("/sign-up", auth.signupFields, auth.signup);
 router.post("/sign-in", auth.signinFields, auth.signin);
-router.get("/:id", cipherController.getUserCiphers);
+router.get("/:id/ciphers", cipherController.getUserCiphers);
 router.get("/me", auth.isAuthenicated, userController.me)
 
 module.exports = router;
