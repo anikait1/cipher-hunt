177d0eb476fbfdebe6f52faae5cf07d6f6a8be27
Anikait <50035624+anikait1@users.noreply.github.com>
2021-03-15 14:58:58 +0530

    Added route for getting user ciphers

--- resources/user/user.route.js
+++ resources/user/user.route.js
@@ -1,10 +1,11 @@
 const userController = require("./user.controller");
+const cipherController = require("../cipher/cipher.controller");
 const router = require("express").Router();
 const auth = require("../../utils/auth");
 
 router.post("/sign-up", auth.signupFields, auth.signup);
 router.post("/sign-in", auth.signinFields, auth.signin);
-router.post("/")
+router.get("/:id", cipherController.getUserCiphers);
 router.get("/me", auth.isAuthenicated, userController.me)
 
 module.exports = router;
